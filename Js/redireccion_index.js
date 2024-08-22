78ac181d9138fe215d74aa5b840a0574ce42fe1a
Danilo <Danilo@gavari.com>
2024-08-22 19:36:56 -0300

    botones

---
+++ Js/redireccion_index.js
@@ -0,0 +1,55 @@
+document.getElementById('final_fantasy').addEventListener('click', function() {
+    // Redirige a la página del juego
+window.location.href = '../final_fantasy.html';
+});
+
+document.getElementById('battle_city0').addEventListener('click', function() {
+    // Redirige a la página del juego
+window.location.href = '../battle_city.html';
+});
+
+document.getElementById('elden_ring').addEventListener('click', function() {
+    // Redirige a la página del juego
+window.location.href = '../elden_ring.html';
+});
+document.getElementById('budokai0').addEventListener('click', function() {
+    // Redirige a la página del juego
+window.location.href = '../budokai_tenkaichi_iii.html';
+});
+
+document.getElementById('supermariobros0').addEventListener('click', function() {
+    // Redirige a la página del juego
+window.location.href = '../mario_bross.html';
+});
+
+document.getElementById('tron0').addEventListener('click', function() {
+    // Redirige a la página del juego
+window.location.href = '../tron.html';
+});
+
+document.getElementById('forzahorizon0')?.addEventListener('click', function() {
+           // Redirige a la página del juego
+    window.location.href = '../forza.html';
+    });
+
+document.getElementById('thecrew')?.addEventListener('click', function() {
+           // Redirige a la página del juego
+    window.location.href = '../the_crew.html';
+    });
+
+document.getElementById('needforspeed')?.addEventListener('click', function() {
+           // Redirige a la página del juego
+           console.log('Needforspeed');
+    window.location.href = '../need_for_speed.html';
+    });
+
+document.getElementById('twistedmetal')?.addEventListener('click', function() {
+           // Redirige a la página del juego
+    window.location.href = '../twister_metal.html';
+    });
+
+
+document.getElementById('granturismo')?.addEventListener('click', function() {
+           // Redirige a la página del juego
+    window.location.href = '../gran_turismo.html';
+    });
